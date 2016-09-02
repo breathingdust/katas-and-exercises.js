1538e2a7f58e1a4cbad635d682c77feba8b2cf4a
Simon Davis <sdavis@paybyphone.com>
2016-09-02 16:49:42 -0700

    couple more little exercises

---
+++ iseven.js
@@ -0,0 +1,9 @@
+Here’s another way to define whether a positive whole number is even or odd:
+
+ Zero is even.
+
+ One is odd.
+
+ For any other number N, its evenness is the same as N - 2.
+
+Define a recursive function isEven corresponding to this description. The function should accept a number parameter and return a Boolean.
