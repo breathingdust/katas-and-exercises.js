af7c7bbed8e34cb9deaa3ff7f0c5e87e5e778139
Simon Davis <simon@breathingdust.com>
2016-09-09 08:02:28 -0700

    couple more list functions

--- list.js
+++ list.js
@@ -5,18 +5,23 @@
 
 function arrayToList(arr){
     var result = null;
-    for(var i = arr.length -1;i >= 0;i++){
-        result = { value: i, rest: result};
+    for(var i = arr.length -1;i >= 0;i--){
+        result = { value: arr[i], rest: result};
     }
     return result;
 }
 
 function listToArray(list){
+    var result = [];
 
+    for(var node = list;node;node = node.rest){
+        result.push(node.value);
+    }
+    return result;
 }
 
 function prepend(element, list){
-
+    return {value:element,rest:list};
 }
 
 function nth(list, number){
--- test/listTest.js
+++ test/listTest.js
@@ -4,14 +4,14 @@ var list = require('./../list');
 
 describe('a list', function(){
     it('should return a list generated from the array', function(){
-        var actual = list.arrayToList([3,5,7]);
+        var actual = list.arrayToList([10,20,30]);
 
         var expected = {
-            value: 7,
+            value: 10,
             rest : {
-                value: 5,
+                value: 20,
                 rest: {
-                    value: 3,
+                    value: 30,
                     rest: null
                 }
             }
@@ -19,4 +19,52 @@ describe('a list', function(){
 
         expect(actual).to.deep.eql(expected);
     })
+
+    it('should return an array generated from the list', function(){
+        var actual = list.listToArray({
+            value: 10,
+            rest : {
+                value: 20,
+                rest: {
+                    value: 30,
+                    rest: null
+                }
+            }
+        });
+
+        var expected = [10,20,30];
+
+        expect(actual).to.deep.eql(expected);
+    })    
+
+    it('should prepend an element to a list', function(){
+        var input = {
+            value: 10,
+            rest : {
+                value: 20,
+                rest: {
+                    value: 30,
+                    rest: null
+                }
+            }
+        };
+
+        var actual = list.prepend(0,input);
+
+        var expected = {
+            value: 0,
+            rest: {
+            value: 10,
+                rest : {
+                    value: 20,
+                    rest: {
+                        value: 30,
+                        rest: null
+                    }
+                }
+            }
+        };
+
+        expect(actual).to.deep.eql(expected);
+    })    
 })
